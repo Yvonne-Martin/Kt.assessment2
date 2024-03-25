d1dac52ffc7e9be812d5c139d8b95c5c39a8a416
Ynonne-Martin <yvonnemartinkatumbi@gmail.com>
2024-03-25 08:32:48 +0300

    added files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+fun main() {
+
+    println(volume(3.14159 ,40))
+    println(arr(45,67))
+}
+
+
+//question 3//
+fun volume (num1:Double,num:Int):Double{
+    var w =(4/3*num1*num*num*num)
+    return w
+}
+//question 2//
+fun arr (num1:Int,num2:Int):Array<Int>{
+    var arr1 = (num1+num2)
+            return arrayOf(arr1)
+
+
+
+
+}
+//Question 1
+
+
